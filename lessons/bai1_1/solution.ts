be24f81f43e5bede8a81de4abd9f434fb7764144
Phạm Văn Bảo Huy <huypham@MacBookPro.lan>
2025-09-05 09:17:20 +0700

    Hoàn thành bài tập 1_1

--- lessons/bai1_1/solution.ts
+++ lessons/bai1_1/solution.ts
@@ -1,3 +1,4 @@
+import crypto from "crypto";
 
 export type Block = {
   index: number;
@@ -7,7 +8,18 @@ export type Block = {
   current_hash: string;
 };
 
-// ✍️ TODO: Viết hàm tại đây
+// 📝 Hàm kiểm tra block hợp lệ
 export function isValidBlock(block: Block): boolean {
-  return false; // Chỉnh lại logic
+  // Ghép dữ liệu cần hash
+  const data =
+    block.index.toString() +
+    block.timestamp +
+    JSON.stringify(block.transactions) +
+    block.previous_hash;
+
+  // Tính SHA256
+  const hash = crypto.createHash("sha256").update(data).digest("hex");
+
+  // So sánh với current_hash
+  return hash === block.current_hash;
 }
